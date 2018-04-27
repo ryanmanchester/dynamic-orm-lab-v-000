10c95233c0533bbaff659f1a52745f8e32031159
Ryan Manchester <ryanm17@gmail.com>
2018-04-27 02:19:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,6 +56,7 @@ class InteractiveRecord
 
   def self.find_by(attribute)
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute} = '#{attribute}'"
+    binding.pry
     DB[:conn].execute(sql)
   end
 
