eb41da1ec91d8c5ad3eb28e56e7e4afb84ad5d54
Ryan Manchester <ryanm17@gmail.com>
2018-04-27 01:19:30 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -7,4 +7,9 @@ class InteractiveRecord
     self.to_s.downcase.pluralize
   end
 
+  def self.column_names
+    DB[:conn].results_as_hash = true
+    sql = "PRAGMA table_info(#{table_name})"
+  end
+
 end
