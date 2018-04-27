efab2eb866f1b820e5ba29659696ca6034930f16
Ryan Manchester <ryanm17@gmail.com>
2018-04-27 02:26:28 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -56,7 +56,6 @@ class InteractiveRecord
 
   def self.find_by(attribute)
     sql = "SELECT * FROM #{self.table_name} WHERE #{attribute.first[0].to_s} = '#{attribute.values.join}'"
-    #binding.pry
     DB[:conn].execute(sql)
   end
 
